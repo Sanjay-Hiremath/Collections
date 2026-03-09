60d793017329d5f2faeb72a5dd4144239dadf2fe
Sanjay Kumar Hiremath <sanjayhiremath897@gmail.com>
2026-03-09 18:21:56 +0530

    moved all the 3 examples files Car.java,Person.java and GenericRepo.java in a single folder GenericEx1

---
+++ GenericsEx1/Person.java
@@ -0,0 +1,17 @@
+package CollectionsGenerics;
+
+public class Person {
+
+	String name;
+	
+	public Person(String name) {
+	this.name=name;	
+	}
+	public void display() {
+		System.out.println(name);
+	}
+	@Override
+	public String toString(){
+		return name;
+	}
+}
